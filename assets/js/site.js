5d4b38f23abc17dbb3fd7a99302b921b0e839f17
Matt Collins <mailmattcollins@gmail.com>
2014-11-15 21:48:39 +0000

    Better smooth scroll js.

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,35 +2,13 @@
  * Site JS
  */
 
-// animate any element (without jQuery)
-function animate (elem, style, unit, from, to, time, prop) {
-    if (!elem) { return; }
-    var start = new Date().getTime();
-    var timer = setInterval(function () {
-            var step = Math.min(1, (new Date().getTime() - start) / time);
-            if (prop) {
-                elem[style] = (from + step * (to - from)) + unit;
-            } else {
-                elem.style[style] = (from + step * (to - from)) + unit;
-            }
-            if (step === 1) { clearInterval(timer); }
-        }, 25);
-    elem.style[style] = from + unit;
-}
-
 // set up smooth scroll
 function initSmoothScroll () {
-    function scrollToEl (e) {
-        e.preventDefault();
-        var target = this.href.split('#')[1];
-        var targetEl = document.querySelector('#' + target);
-        animate(document.body, 'scrollTop', '', 0, targetEl.offsetTop, 150, true);
-    }
-
-    var internalAnchors = document.querySelectorAll('a[href^="#"]');
-    for (var count = internalAnchors.length - 1; count >= 0; count--) {
-        internalAnchors[count].addEventListener('click', scrollToEl);
-    }
+    smoothScroll.init({
+        speed: 750,
+        easing: 'easeOutCubic',
+        updateURL: false
+    });
 }
 
 // init all
