db82df59cd33d9c6d67c0dba5dbbc3d0050f3e6d
Matt Collins <mailmattcollins@gmail.com>
2014-12-16 12:08:39 +0000

    Header height js.

--- assets/js/site.js
+++ assets/js/site.js
@@ -11,9 +11,19 @@ function initSmoothScroll () {
     });
 }
 
+// init header
+function initHeader () {
+    var header = document.getElementsByTagName('header')[0];
+
+    if (window.innerHeight > header.offsetHeight) {
+        header.style['height'] = '' + window.innerHeight + 'px';
+    }
+}
+
 // init all
 function init () {
     initSmoothScroll();
+    initHeader();
 }
 
 // go!
